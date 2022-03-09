c478f8e2f309d9ff6f8cd390fc8d9aa642bc2543
Belta-shazzar <oguejioforchinonso20@gmail.com>
2022-03-09 19:40:34 +0100

    MySQL file added

---
+++ cutomer.sql
@@ -0,0 +1,12 @@
+id,account_name,phone_number,dob,email,account_number,account_balance,password
+1,"John Doe",07012345678,01-03-1995,johndoe@gmail.com,866609289,5500,2022
+2,"Sarah Smith",08012345678,13-08-1997,sarahsmith@gmail.com,199047544,83450,1997
+3,"Dave Klei",08123456789,12-12-1990,daveklei@email.com,570613581,45000,2209
+4,"Shasha Bim",09012345678,01-02-2001,shashababy@yahoo.com,290375474,6000,2010
+5,"Joel Sam",09012384756,01-05-2003,joelsam@gmail.com,666798462,33500,1121
+6,"Amy Aimes",08176438432,22-04-1992,amyaimes@gmail.com,1151122715,4000,3991
+7,"Simon Jack",08147827329,28-02-1989,simonjack@gmail.com,115648132,8300,1082
+9,ceo,08128349573,29-03-2003,ceo@gmail.com,863463633,2000,5783
+11,"Daniel Okoronkwo",0909193407,02-09-2001,daniel@gmail.com,658154467,1580,9193
+13,"Daniel Jewel",08137373737,27-08-2002,jewel@gmail.com,778808787,192000,1919
+14,"My Test",07089436870,20-02-2000,nielnonso@gmail.com,907457595,83000,2020
